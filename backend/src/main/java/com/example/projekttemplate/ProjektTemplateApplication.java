fe483764654c86a0a0c498e2dd2e4b176aecab23
BastiK81 <basti.knaack@gmail.com>
2022-01-14 13:08:29 +0100

    first Commit

---
+++ backend/src/main/java/com/example/projekttemplate/ProjektTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektTemplateApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/projekttemplate/ProjektTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
